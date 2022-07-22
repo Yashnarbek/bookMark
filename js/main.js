d40f1ef007c6bb8306c57fa560b897d8df04d21a
Yashnarbek <ymqayumov@gmail.com>
2022-07-22 18:09:07 +0300

    Add section in js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ for(let i of details){
         console.log() 
     })
 }
+
+
+
+let link = document.querySelectorAll(".feature-list__link")
+let item = document.querySelectorAll(".feature__end")
+
+link.forEach(function(elm){
+
+    elm.addEventListener("click", function(evt){
+        evt.preventDefault()
+
+        link.forEach(function(choosen){ 
+            choosen.parentElement.classList.remove("feature-list__item--active")
+        })
+        
+        elm.parentElement.classList.add("feature-list__item--active")
+
+        item.forEach(function(textchoosen){
+            textchoosen.classList.remove("feature-bottom--wiew")
+        })
+        document.querySelector(elm.getAttribute("href")).classList.add("feature-bottom--wiew")
+    })
+})
